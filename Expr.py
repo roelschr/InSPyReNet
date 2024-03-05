fdf1c1ed1cbd350fc54b6d8cc423be0a58b2ed90
Igor Hoelscher <igor.hoelscher@gmail.com>
2024-03-05 06:12:02 -0800

    Convert training func to ray.train

--- Expr.py
+++ Expr.py
@@ -2,13 +2,27 @@ import torch.cuda as cuda
 
 from utils.misc import *
 from run import *
+import ray
+from ray.train.torch import TorchTrainer
+from ray.train import ScalingConfig
 
 if __name__ == "__main__":
     args = parse_args()
     opt = load_config(args.config)
     
-    train(opt, args)
-    cuda.empty_cache()
+    ray.init(
+        runtime_env={
+            "pip": "requirements.txt",
+        },
+    )
+    scaling_config = ScalingConfig(num_workers=4, use_gpu=True)
+    trainer = TorchTrainer(
+        train,
+        train_loop_config={"args": args, "opt": opt},
+        scaling_config=scaling_config
+    )
+    result = trainer.fit()
+
     if args.local_rank <= 0:
         test(opt, args)
         evaluate(opt, args)
